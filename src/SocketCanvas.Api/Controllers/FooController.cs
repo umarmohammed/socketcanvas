22510947cd79a8709968b7f3ef251d696a20c0f3
Umar Mohammed <umohammed@caci.co.uk>
2019-11-02 17:40:06 +0000

    add dotnet core project

---
+++ src/SocketCanvas.Api/Controllers/FooController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocketCanvas.Api.Controllers
+{
+    public class FooController : ControllerBase
+    {
+        [Route("api/foo")]
+        public string Foo()
+        {
+            return "foo";
+        }
+    }
+}
---
+++ src/SocketCanvas.Api/Startup.cs
@@ -0,0 +1,38 @@
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SocketCanvas.Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseCors(options =>
+            {
+                options.AllowAnyOrigin();
+                options.AllowAnyHeader();
+                options.AllowAnyMethod();
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
